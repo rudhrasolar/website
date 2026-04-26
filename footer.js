c5c76cea02a1588281a20f127b7355d9362d3d60
vvk14 <vikupatel2001@gmail.com>
2026-04-26 20:03:49 +0530

    Footer Content

--- footer.js
+++ footer.js
@@ -13,7 +13,7 @@
           <a href="index.html" class="footer-logo-link">
             <img src="/images/logo.png" alt="Rudhra Logo" style="max-width: 180px; margin-bottom: 10px;"> 
           </a>
-          <p class="brand-desc">We started as aspiring entrepreneurs committed to achieving excellence in our services and ensuring the satisfaction of our clients. Perseverance and consistency have led us to our present: providing services to leading companies and maintaining solar plants.</p>
+          <p class="brand-desc">Rudra Solar Greentech is India's trusted solar O&M partner, committed to maximizing the performance and longevity of your solar assets through expert maintenance, skilled manpower, and cutting-edge technology.</p>
         </div>
 
         <div class="footer-col accordion-col">
@@ -57,20 +57,19 @@
               <div class="dept-block">
                 <h6>Sales & Marketing:</h6>
                 <a href="tel:+919509224116"><i class="fas fa-phone-alt"></i> +91 95092 24116</a>
-                <a href="mailto:Premsinghjhala@Rudhramanagements.com"><i class="fas fa-envelope"></i> Premsinghjhala@Rudhramanagements.com</a>
+                <a href="mailto:sales@rudhragreen.com"><i class="fas fa-envelope"></i> sales@rudhragreen.com</a>
               </div>
 
               <div class="dept-block">
                 <h6>Procurement:</h6>
                 <a href="tel:+919828344455"><i class="fas fa-phone-alt"></i> +91 98283 44455</a>
-                <a href="mailto:Giriraj1rathod@yahoo.com"><i class="fas fa-envelope"></i> Giriraj1rathod@yahoo.com</a>
-                <a href="mailto:vikram.rathore@Rudhramanagements.com"><i class="fas fa-envelope"></i> vikram.rathore@Rudhramanagements.com</a>
+                <a href="mailto:procurement@rudhragreen.com"><i class="fas fa-envelope"></i> procurement@rudhragreen.com</a>
               </div>
 
               <div class="dept-block">
                 <h6>HR & Recruitment:</h6>
                 <a href="tel:+919116729736"><i class="fas fa-phone-alt"></i> +91 91167 29736</a>
-                <a href="mailto:hr@Rudhramanagements.com"><i class="fas fa-envelope"></i> hr@Rudhramanagements.com</a>
+                <a href="mailto:hr@rudhramanagements.com"><i class="fas fa-envelope"></i> hr@rudhramanagements.com</a>
               </div>
 
             </div>
